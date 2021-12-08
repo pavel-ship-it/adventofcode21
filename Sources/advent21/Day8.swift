2e0c52b810b21a070b6377edac47130747d94748
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2021-12-08 08:05:47 +0000

    Day8 small fix

--- Sources/advent21/Day8.swift
+++ Sources/advent21/Day8.swift
@@ -13,8 +13,8 @@ class Task15: Task {
 }
 
 class Task16: Task {
-    func cont(_ a: [String.Element], _ b: [String.Element]) -> Int {
-        b.filter { !a.contains($0) }.count
+    func cont(_ a: [String.Element], _ b: [String.Element]?) -> Int {
+        b!.filter { !a.contains($0) }.count
     }
     func decoder(_ code: [[String.Element]]) -> Int {
         var d = [[String.Element]?](repeating: nil, count: 10)
@@ -22,21 +22,14 @@ class Task16: Task {
         d[4] = code.first { $0.count == 4 }
         d[7] = code.first { $0.count == 3 }
         d[8] = code.first { $0.count == 7 }
-        if let d1 = d[1], let d4 = d[4] {
-            d[3] = code.first { $0.count == 5 && cont($0, d1) == 0 }
-            d[5] = code.first { $0.count == 5 && cont($0, d1) == 1 && cont($0, d4) == 1 }
-            if let d3 = d[3], let d5 = d[5] {
-                d[2] = code.first { $0.count == 5 && cont($0, d3) != 0 && cont($0, d5) != 0 }
-            }
-            d[6] = code.first { $0.count == 6 && cont($0, d1) == 1 && cont($0, d1) != 0 }
-        }
-        if let d1 = d[1], let d4 = d[4] {
-            d[9] = code.first { $0.count == 6 && cont($0, d4) == 0 }
-            d[0] = code.first { $0.count == 6 && cont($0, d4) == 1 && cont($0, d1) == 0 }
-        }
+        d[3] = code.first { $0.count == 5 && cont($0, d[1]) == 0 }
+        d[5] = code.first { $0.count == 5 && cont($0, d[1]) == 1 && cont($0, d[4]) == 1 }
+        d[2] = code.first { $0.count == 5 && cont($0, d[3]) != 0 && cont($0, d[5]) != 0 }
+        d[6] = code.first { $0.count == 6 && cont($0, d[1]) == 1 }
+        d[9] = code.first { $0.count == 6 && cont($0, d[4]) == 0 }
+        d[0] = code.first { $0.count == 6 && cont($0, d[4]) == 1 && cont($0, d[1]) == 0 }
         return (d.firstIndex(of: code[10])! * 1000 + d.firstIndex(of: code[11])! * 100 + d.firstIndex(of: code[12])! * 10 + d.firstIndex(of: code[13])! * 1) as Int
     }
-    
     func calc(_ inputFile: String) -> Int {
         fileData(inputFile)
             .map {
