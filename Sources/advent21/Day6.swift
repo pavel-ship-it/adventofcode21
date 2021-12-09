fdd3f3104457387f77cd1769755cf73fff36f1ef
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2021-12-09 17:39:57 +0000

    Better Day 6

--- Sources/advent21/Day6.swift
+++ Sources/advent21/Day6.swift
@@ -1,29 +1,28 @@
-func getFishes(_ input: String) -> [UInt] {
-    input.components(separatedBy: ",").map { Int($0)! }
+class Task11: Task {
+    func getFishes(_ input: String) -> [UInt] {
+        input.components(separatedBy: ",").map { Int($0)! }
         .reduce([UInt](repeating: 0, count: 9)) {
             var ret = $0
             ret[$1] += 1
             return ret
         }
-}
-func grow(_ turns: Int, _ fishes: [UInt]) -> Int {
-    var fishes = fishes
-    (0..<turns).forEach { _ in
-        fishes[7] += fishes[0]
-        fishes.append(fishes[0])
-        fishes.remove(at: 0)
     }
-    return Int(exactly: fishes.reduce(0, +))!
-}
-class Task11: Task {
+    func grow(_ turns: Int, _ fishes: [UInt]) -> Int {
+        var fishes = fishes
+        (0..<turns).forEach { _ in
+            fishes[7] += fishes[0]
+            fishes.append(fishes[0])
+            fishes.remove(at: 0)
+        }
+        return Int(exactly: fishes.reduce(0, +))!
+    }
     func calc(_ inputFile: String) -> Int {
         let fishes = getFishes(fileData(inputFile)[0])
         return grow(80, fishes)
     }
 }
-
-class Task12: Task {
-    func calc(_ inputFile: String) -> Int {
+class Task12: Task11 {
+    override func calc(_ inputFile: String) -> Int {
         let fishes = getFishes(fileData(inputFile)[0])
         return grow(256, fishes)
     }
