b8f344a7c93ab1be9ebd3be5cff9964f2bbf3b5e
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2021-12-15 22:07:47 +0000

    Day 15 clean

--- Sources/advent21/Day15.swift
+++ Sources/advent21/Day15.swift
@@ -2,43 +2,15 @@ class Task29: Task {
     var maxV = 0
     var field = [[Int]]()
     var costs = [[Int]]()
-    func cost(_ p: Point) -> Int {
-        if costs[p.y][p.x] != 0 {
-            return costs[p.y][p.x]
-        }
-        if p.x >= maxV-1 , p.y >= maxV-1 {
-            costs[p.y][p.x] = field[p.y][p.x]
-            return costs[p.y][p.x]
-        }
-        var cX = Int.max
-        var cY = Int.max
-        if p.x < maxV-1 {
-            let d = Point(x: p.x+1, y: p.y)
-            cX = field[p.y][p.x] + cost(d)
-        }
-        if p.y < maxV-1 {
-            let d = Point(x: p.x, y: p.y+1)
-            cY = field[p.y][p.x] + cost(d)
-        }
-        costs[p.y][p.x] = min(cX, cY)
-        return costs[p.y][p.x]
-    }
-    func loadMap(_ inputFile: String) {
-        field = fileData(inputFile).map { $0.map { Int(String($0))! } }
-        maxV = field.count
-        field[0][0] = 0
-        costs = [[Int]](repeating: [Int](repeating: 0, count: maxV), count: maxV)
-    }
+    var nextVisit: [Point] = [Point(x: 0, y: 0)]
+    var isVisited = [[Bool]]()
     func calc(_ inputFile: String) -> Int {
         loadMap(inputFile)
-        _ = cost(Point(x: 0, y: 0))
-        return costs[0][0]
-    }
-}
-
-class Task30: Task29 {
-    override func calc(_ inputFile: String) -> Int {
-        loadMap(inputFile)
+        maxV = field.count
+        costs = [[Int]](repeating: [Int](repeating: Int.max, count: maxV), count: maxV)
+        isVisited = [[Bool]](repeating: [Bool](repeating: false, count: maxV), count: maxV)
+        field[0][0] = 0
+        costs[0][0] = 0
         while nextVisit.count > 0 {
             let pt = nextVisit.remove(at: 0)
             if isVisited[pt.y][pt.x] {
@@ -48,6 +20,35 @@ class Task30: Task29 {
         }
         return costs[maxV-1][maxV-1]
     }
+    func compare(_ a: Point, _ b: Point) {
+        if costs[a.y][a.x] > costs[b.y][b.x]+field[a.y][a.x] {
+            costs[a.y][a.x] = costs[b.y][b.x]+field[a.y][a.x]
+            isVisited[a.y][a.x] = false
+            nextVisit.append(a)
+        }
+    }
+    func cost(_ p: Point) -> Int {
+        if p.x > 0 {
+            compare(Point(x: p.x-1, y: p.y), p)
+        }
+        if p.y > 0 {
+            compare(Point(x: p.x, y: p.y-1), p)
+        }
+        if p.x < maxV-1 {
+            compare(Point(x: p.x+1, y: p.y), p)
+        }
+        if p.y < maxV-1 {
+            compare(Point(x: p.x, y: p.y+1), p)
+        }
+        isVisited[p.y][p.x] = true
+        return costs[p.y][p.x]
+    }
+
+    func loadMap(_ inputFile: String) {
+        field = fileData(inputFile).map { $0.map { Int(String($0))! } }
+    }
+}
+class Task30: Task29 {
     override func loadMap(_ inputFile: String) {
         let tmpField = fileData(inputFile).map { $0.map { Int(String($0))! } }
         let len = tmpField.count
@@ -65,56 +66,5 @@ class Task30: Task29 {
                 }
             }
         }
-        maxV = field.count
-        costs = [[Int]](repeating: [Int](repeating: Int.max, count: maxV), count: maxV)
-        isVisited = [[Bool]](repeating: [Bool](repeating: false, count: maxV), count: maxV)
-        field[0][0] = 0
-        costs[0][0] = 0
-    }
-    var nextVisit: [Point] = [Point(x: 0, y: 0)]
-    var isVisited = [[Bool]]()
-    func compare(_ a: Point, _ b: Point) {
-        if costs[a.y][a.x] > costs[b.y][b.x]+field[a.y][a.x] {
-            costs[a.y][a.x] = costs[b.y][b.x]+field[a.y][a.x]
-            isVisited[a.y][a.x] = false
-            nextVisit.append(a)
-        }
-
-    }
-    override func cost(_ p: Point) -> Int {
-        if p.x > 0 {
-            let d = Point(x: p.x-1, y: p.y)
-            compare(d, p)
-//            if !isVisited[d.y][d.x] {
-//                costs[d.y][d.x] = min(costs[d.y][d.x], costs[p.y][p.x]+field[d.y][d.x])
-//                nextVisit.append(d)
-//            }
-        }
-        if p.y > 0 {
-            let d = Point(x: p.x, y: p.y-1)
-            compare(d, p)
-//            if !isVisited[d.y][d.x] {
-//                costs[d.y][d.x] = min(costs[d.y][d.x], costs[p.y][p.x]+field[d.y][d.x])
-//                nextVisit.append(d)
-//            }
-        }
-        if p.x < maxV-1 {
-            let d = Point(x: p.x+1, y: p.y)
-            compare(d, p)
-//            if !isVisited[d.y][d.x] {
-//                costs[d.y][d.x] = min(costs[d.y][d.x], costs[p.y][p.x]+field[d.y][d.x])
-//                nextVisit.append(d)
-//            }
-        }
-        if p.y < maxV-1 {
-            let d = Point(x: p.x, y: p.y+1)
-            compare(d, p)
-//            if !isVisited[d.y][d.x] {
-//                costs[d.y][d.x] = min(costs[d.y][d.x], costs[p.y][p.x]+field[d.y][d.x])
-//                nextVisit.append(d)
-//            }
-        }
-        isVisited[p.y][p.x] = true
-        return costs[p.y][p.x]
     }
 }
